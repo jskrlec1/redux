470d02925a5dd13441b0223fe6a2538e784dedc5
jskrlec1 <josip.skrlecvk@gmail.com>
2023-11-29 13:11:14 +0100

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { connect } from 'react-redux';
+import { addToCount, subtractFromCount, setCount } from './redux';
 import './App.css';
 
-function App() {
+function App({ count, addOne, subtractOne, reset }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>
+        <span>
+          <button onClick={subtractOne}> - </button>
+          <span className="counter"> Broj: {count} </span>
+          <button onClick={addOne}> + </button>
+        </span>
+      </p>
+      <p>
+        <button onClick={reset}>Resetiraj broj</button>
+      </p>
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    count: state,
+  };
+}
+
+const mapDispatchToProps = {
+  addOne: () => addToCount(1),
+  subtractOne: () => subtractFromCount(1),
+  reset: () => setCount(0),
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
---
+++ src/redux.js
@@ -0,0 +1,28 @@
+const ADD_COUNT = 'ADD_COUNT';
+const SUBTRACT_COUNT = 'SUBTRACT_COUNT';
+const SET_COUNT = 'SET_COUNT';
+
+export function addToCount(number = 1) {
+  return { type: ADD_COUNT, number };
+}
+
+export function subtractFromCount(number = 1) {
+  return { type: SUBTRACT_COUNT, number };
+}
+
+export function setCount(count = 0) {
+  return { type: SET_COUNT, count };
+}
+
+export function reducer(state = 0, action) {
+  switch (action.type) {
+    case ADD_COUNT:
+      return state + action.number;
+    case SUBTRACT_COUNT:
+      return state - action.number;
+    case SET_COUNT:
+      return action.count;
+    default:
+      return state;
+  }
+}
